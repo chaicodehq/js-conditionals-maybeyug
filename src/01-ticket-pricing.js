f574263cb1f4ab2ed3188b4c297b4dc4ba7daaaf
Yug Saini <41270627+Yug-Singh@users.noreply.github.com>
2026-02-09 00:27:38 +0530

    Complete 01 02 03 labs of conditionals

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,29 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+  if (age >= 0) {
+    if (age >= 0 && age <= 12) {
+      if (isWeekend) {
+        return 8 + 3;
+      }
+      return 8;
+    } else if (age >= 13 && age <= 17) {
+      if (isWeekend) {
+        return 12 + 3;
+      }
+      return 12;
+    } else if (age >= 18 && age <= 59) {
+      if (isWeekend) {
+        return 15 + 3;
+      }
+      return 15;
+    } else {
+      if (isWeekend) {
+        return 10 + 3;
+      }
+      return 10;
+    }
+  } else {
+    return -1;
+  }
 }
